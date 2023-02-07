73949d14b91f1eddfea422c5fed0fab18de597b4
KibaDima1 <t89048439997@mail.ru>
2023-02-07 22:16:32 +0500

    №3 осталось исправить передачу статуса

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
-function start()
+local function start()
     return coroutine.create(function()
         while true do
             print("Введите число")
-            state_detail = {number = 0, detail = io.read()}
+            state_detail = {state = true, detail = io.read()}
             coroutine.yield()
         end
     end)
@@ -18,40 +18,60 @@ local function integer_or_not()
 end
 
 -- шаг первый (проверка на то, являются ли введёные пользователем данные числом)
-local function step1_number_or_not(start)
+local function step1_number_or_not()
     return coroutine.create(function()
         while true do
-            coroutine.resume(start)
-            tonumber(state_detail.detail)
-            coroutine.yield()
+            local status
+            coroutine.resume(starts)
+            if ((tonumber(state_detail.detail))) then
+                status = true
+            else
+                status = false
+            end
+            print(status)
+            coroutine.yield(status)
         end
     end)
 end
 
 -- шаг второй(взятие квадратного корня)
-local function step2_sqrt(step1)
+local function step2_sqrt()
     return coroutine.create(function()
         while true do
-            coroutine.resume(step1)
-            state_detail.detail = math.sqrt(state_detail.detail)
-            coroutine.yield()
+            local status = coroutine.resume(step1)
+            print(status)
+            if status == true then
+                state_detail.detail = math.sqrt(state_detail.detail)
+                status = integer_or_not()
+            end
+            coroutine.yield(status)
         end
     end)
 end
 
 -- шаг третий(деление на 3)
-local function step3_division_by_3(step2)
+local function step3_division_by_3()
     while true do
-        coroutine.resume(step2)
-        state_detail.number = state_detail.number + 1
-        print(state_detail.detail)
+        local status = coroutine.resume(step2)
+        print("step3")
+        if status == true then
+            state_detail.detail = state_detail.detail / 3
+            status = integer_or_not()
+            print(status)
+            if status ~= true then
+                print("система сломалась, если хотите продолжить, нажмите любую клавишу")
+                io.read() 
+            end
+        end
     end
 end
 
-start = start()
+state_detail = {state = true, detail = 0}
 
-step1 = step1_number_or_not(start)
+starts = start()
 
-step2 = step2_sqrt(step1)
+step1 = step1_number_or_not()
 
-step3_division_by_3(step2)
+step2 = step2_sqrt()
+
+step3_division_by_3()
