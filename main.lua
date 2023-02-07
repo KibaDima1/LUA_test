83f3965f46afdebaf4f84e739127f419e660e0d1
KibaDima1 <t89048439997@mail.ru>
2023-02-07 20:33:06 +0500

    № 2 добавлены исполнительные механизмы

--- main.lua
+++ main.lua
@@ -1,16 +1,57 @@
-print("Мы начинаем")
+function start()
+    return coroutine.create(function()
+        while true do
+            print("Введите число")
+            state_detail = {number = 0, detail = io.read()}
+            coroutine.yield()
+        end
+    end)
+end
 
-print("Введите число")
-
-local number = io.read()
-
-function step1_sqrt(num)
-    a = math.sqrt(num)
-    if a % 2 == 0 then
-        print("true")
+-- проверка на целостность числа
+local function integer_or_not()
+    if state_detail.detail % 1 ~= 0 then
+        return false
     else
-        print("false")
+        return true
     end
 end
 
-step1_sqrt(number)
+-- шаг первый (проверка на то, являются ли введёные пользователем данные числом)
+local function step1_number_or_not(start)
+    return coroutine.create(function()
+        while true do
+            coroutine.resume(start)
+            tonumber(state_detail.detail)
+            coroutine.yield()
+        end
+    end)
+end
+
+-- шаг второй(взятие квадратного корня)
+local function step2_sqrt(step1)
+    return coroutine.create(function()
+        while true do
+            coroutine.resume(step1)
+            state_detail.detail = math.sqrt(state_detail.detail)
+            coroutine.yield()
+        end
+    end)
+end
+
+-- шаг третий(деление на 3)
+local function step3_division_by_3(step2)
+    while true do
+        coroutine.resume(step2)
+        state_detail.number = state_detail.number + 1
+        print(state_detail.detail)
+    end
+end
+
+start = start()
+
+step1 = step1_number_or_not(start)
+
+step2 = step2_sqrt(step1)
+
+step3_division_by_3(step2)
